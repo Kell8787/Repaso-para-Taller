832dec5f9e9a020703c31f347677e7bc8b161018
Kell8787 <00083121@uca.edu.sv>
2024-05-03 20:07:39 -0600

    Agregando Delete y mejor interfaz

--- Practicando/app/src/main/java/com/todolist/practicando/MainActivity.kt
+++ Practicando/app/src/main/java/com/todolist/practicando/MainActivity.kt
@@ -1,37 +1,52 @@
 package com.todolist.practicando
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.todolist.practicando.ui.theme.PracticandoTheme
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Date
+import java.util.Locale
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,9 +66,11 @@ class MainActivity : ComponentActivity() {
 
 data class Task(
     val title: String,
-    val description: String
+    val description: String,
+    val createdAt: Date
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MyFuntion (){
     Column (
@@ -73,27 +90,29 @@ fun MyFuntion (){
             mutableStateOf("")
         }
 
-        val list: MutableList<Task> = remember {
-            mutableListOf()
+        val list: SnapshotStateList<Task> = remember {
+            SnapshotStateList()
         }
+
+
 
         Column (
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp)
-                .weight(1f),
+                .weight(0.6f),
             horizontalAlignment = Alignment.CenterHorizontally
 
         ){
-            Text(modifier = Modifier.padding(10.dp),text = "Add Tittle", style = TextStyle(fontSize = 20.sp))
+            Text(modifier = Modifier.padding(10.dp),text = "Add Tittle", fontSize = 20.sp)
             TextField(tittle.value, onValueChange = { tittle.value = it } )
-            Text(modifier = Modifier.padding(10.dp), text = "Add Description", style = TextStyle(fontSize = 20.sp))
+            Text(modifier = Modifier.padding(10.dp), text = "Add Description", fontSize = 20.sp)
             TextField(description.value, onValueChange = { description.value = it } )
             Button(
                 modifier = Modifier
                     .padding(10.dp),
                 onClick = {
-                list.add(Task(tittle.value, description.value))
+                list.add(Task(tittle.value, description.value, createdAt = Date.from(Instant.now())))
                     tittle.value = ""
                     description.value = ""
 
@@ -111,33 +130,41 @@ fun MyFuntion (){
                 Row (
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(10.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ){
-                    Card (
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
+                    Column (
+                        modifier = Modifier.weight(1f)
 
                     ){
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            color = Color.Black,
-                            fontSize = 16.sp,
-                            text = item.title
+                        Text(text = SimpleDateFormat("HH:mm:aa, dd/MM", Locale.ENGLISH).format(item.createdAt),
+                            fontSize = 12.sp,
+                            color = Color.LightGray
                         )
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            color = Color.LightGray,
-                            text = item.description
-                        )
+                        Text(text = item.title, fontSize = 20.sp, color = Color.White)
+                        Text(text = item.description, fontSize = 15.sp, color = Color.Black)
                     }
+                    IconButton(onClick = {
+                            list.remove(item)
+                    }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_delete_24),
+                            contentDescription = "Delete Icon",
+                            tint = Color.White
+                            )
+                    }
+
                 }
             }
         }
-
     }
+
 }
 
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 private fun MyFuntionPreview(){
